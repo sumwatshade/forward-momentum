1d5c92fd1b15fee1747e6c53b4cc7e3326d3f841
lshadler <lshadler13@gmail.com>
2020-06-08 11:42:39 -0700

    fix: gradient shows more info

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -16,18 +16,19 @@
 }
 
 .state {
-    border: 1px solid black;
-    border-radius: 3px;
+  border: 1px solid black;
+  border-radius: 3px;
     
 	width: 50px;
 	height: 50px;
 
-    display: flex;
-    flex-direction: column;
-    place-items: center;
-    margin: 20px;
-    padding: 20px;
-    font-size: 2rem;
+  display: flex;
+  flex-direction: column;
+  place-items: center;
+  padding: 20px;
+  font-size: 2rem;
+
+  background: #ffffff;
 }
 
 .rep {
@@ -38,6 +39,13 @@
 .dem {
   color: #3C3B6E;
   border-color: #3C3B6E;
+}
+
+.border-wrap {
+  max-width: 250px;
+  position: relative;
+  padding: 3px;
+  margin: 20px;
 }
 
 @media (max-width: 620px) {
@@ -58,9 +66,13 @@
 				tell Sapper to load the data for the page as soon as
 				the user hovers over the link or taps it, instead of
 				waiting for the 'click' event -->
-		<div in:fade class={`state ${state.numDem > state.numRep ? 'dem' : 'rep'}`}>
-            <a rel='prefetch' href='representatives/{state.id}'>{state.id.toUpperCase()}</a>
-        </div>
+		<div in:fade class={`border-wrap`} style={`
+      background: linear-gradient(315deg,rgba(178,34,52,1) 0%, rgba(178,34,52,1) ${parseInt((100.0 * state.numRep)/(state.numRep + state.numDem))}%,  rgba(60,59,110,1) 100%)
+    `}>
+      <div class={`state ${state.numDem > state.numRep ? 'dem' : 'rep'}`}>
+          <a rel='prefetch' href='representatives/{state.id}'>{state.id.toUpperCase()}</a>
+      </div>
+    </div>
 	{/each}
     </div>    
 </div>
