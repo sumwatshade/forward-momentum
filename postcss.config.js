58d5acea60028794585a91f0210c4ba72b1ab30f
lshadler <lshadler13@gmail.com>
2020-06-12 16:51:57 -0700

    fix: update snaps

--- postcss.config.js
+++ postcss.config.js
@@ -3,7 +3,7 @@ const tailwindcss = require('tailwindcss');
 const purgecss = require('@fullhuman/postcss-purgecss')({
   content: ['./src/**/*.svelte', './src/**/*.html'],
   defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
-  whitelistPatterns: [/democrat$/, /republican$/],
+  whitelistPatterns: [/democrat$/, /republican$/, /libertarian$/, /independent$/],
 });
 
 module.exports = {
